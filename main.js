32a49031dfd0dbd7ac69b555793faf2af66ed6e9
Ihiechi <ihechibethel5@gmail.com>
2023-04-07 17:00:41 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,16 @@
+function getInputValue(){
+    // Selecting the input element and get its value 
+    var inputVal = document.getElementById("inputTodo").value;
+    
+    document.getElementById ("med").innerHTML = inputVal;
+    // Displaying the value
+    
+
+ 
+
+}
+
+getInputValue();
+
+// To submit the input 
+
